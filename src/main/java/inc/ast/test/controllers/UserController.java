02d3d9fb820e54854e930667445d542284a252bb
Nikita <nikita1998-15@mail.ru>
2022-06-30 12:05:05 +0300

    added User Search

--- src/main/java/inc/ast/test/controllers/UserController.java
+++ src/main/java/inc/ast/test/controllers/UserController.java
@@ -1,25 +1,43 @@
 package inc.ast.test.controllers;
 
 import inc.ast.test.entitys.user.User;
+import inc.ast.test.repos.UserFilterRepo;
 import inc.ast.test.repos.UserRepo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
     UserRepo userRepo;
+    UserFilterRepo userFilterRepo;
 
-    public UserController(UserRepo userRepo) {
+    public UserController(UserRepo userRepo, UserFilterRepo userFilterRepo) {
         this.userRepo = userRepo;
+        this.userFilterRepo = userFilterRepo;
     }
 
     @GetMapping("/list")
     public String userList(Model model) {
         Iterable<User> userList = userRepo.findAll();
         model.addAttribute("users", userList);
+        return "userList";
+    }
+
+    @PostMapping("filter")
+    public String filterByUsername(@RequestParam String filter, Model model) {
+            Iterable <User> users;
+
+        if (filter != null && !filter.isEmpty()) {
+            users = userFilterRepo.findByUsername(filter);
+        } else {
+            users = userRepo.findAll();
+        }
+        model.addAttribute("users", users);
         return "userList";
     }
 
