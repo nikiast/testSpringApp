5f4d93b472405da5b92c8b0db6e9eac7935f77f1
Никита <nikita1998-15@mail.ru>
2022-07-06 22:28:24 +0300

    added session context for tomorrow

--- src/main/java/inc/ast/test/controller/MainPageController.java
+++ src/main/java/inc/ast/test/controller/MainPageController.java
@@ -1,7 +1,11 @@
 package inc.ast.test.controller;
 
 import inc.ast.test.model.product.Product;
+import inc.ast.test.model.user.User;
 import inc.ast.test.repository.ProductRepo;
+import inc.ast.test.repository.UserServiceRepo;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainPageController {
     private ProductRepo productRepo;
+    private UserServiceRepo userServiceRepo;
 
     public MainPageController(ProductRepo productRepo) {
         this.productRepo = productRepo;
@@ -19,6 +24,23 @@ public class MainPageController {
         Iterable<Product> productList = productRepo.findAll();
         model.addAttribute("products", productList);
         return "main";
+    }
+
+    @GetMapping("/ses")
+    public String indexx(Model model) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        String username;
+        User user = (User) principal;
+//        if (principal instanceof UserDetails) {
+//            username = ((UserDetails)principal).getUsername();
+//        } else {
+//            username = principal.toString();
+//        }
+//
+//        User user = userServiceRepo.findByUsername(username);
+        model.addAttribute("user", user);
+        return "ses";
     }
 
 }
