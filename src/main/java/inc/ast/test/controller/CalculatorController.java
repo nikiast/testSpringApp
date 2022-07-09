a7fc715fbac923cf542fa8dd5ea93c342225ae42
Nikita <nikita1998-15@mail.ru>
2022-07-09 13:09:25 +0300

    added calc

---
+++ src/main/java/inc/ast/test/controller/CalculatorController.java
@@ -0,0 +1,38 @@
+package inc.ast.test.controller;
+
+import inc.ast.test.model.user.Role;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/calc")
+public class CalculatorController {
+
+    @GetMapping
+    public String calculator(){
+        return "calculator/calc";
+    }
+
+    @PostMapping
+    public String calculatorPost(@RequestParam(name="value1") Double value1,
+                                 @RequestParam(name="value2") Double value2,
+                                 @RequestParam(name="operation") String operation,
+                                 Model model){
+        if(!(value2 == 0 && operation.equals("/"))){
+            switch (operation) {
+                case "+" -> model.addAttribute("result", value1+value2);
+                case "-" -> model.addAttribute("result", value1-value2);
+                case "*" -> model.addAttribute("result", value1*value2);
+                case "/" -> model.addAttribute("result", value1/value2);
+            }
+        }else {
+            model.addAttribute("result", "NaN");
+        }
+        return "calculator/calc";
+    }
+
+}
