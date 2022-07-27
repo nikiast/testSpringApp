e941afb31e3c50b9dd4fba4b3e5a27459e3b4c2e
Nikita <nikita1998-15@mail.ru>
2022-07-27 13:59:09 +0300

    add validator

---
+++ src/main/java/inc/ast/test/util/UserValidator.java
@@ -0,0 +1,29 @@
+package inc.ast.test.util;
+
+import inc.ast.test.model.user.User;
+import inc.ast.test.repository.UserRepo;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class UserValidator implements Validator {
+    private final UserRepo userRepo;
+
+    public UserValidator(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return User.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        User user = (User) target;
+        if (userRepo.findByUsername(user.getUsername()) != null){
+            errors.rejectValue("username", "", "this username is exist");
+        }
+    }
+}
